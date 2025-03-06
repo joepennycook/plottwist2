316c00af2c559f12f50bcef9fe9234e8ad58bff6
ea24925 <joseph.pennycook@bristol.ac.uk>
2025-03-06 15:12:56 +0000

    add test for symbol_scale_manual

--- tests/testthat/test-geom_symbol.R
+++ tests/testthat/test-geom_symbol.R
@@ -4,3 +4,12 @@ test_that("geom_symbol works", {
                                               ggplot2::aes(x = cyl, y = mpg)) +
                                 geom_symbol())
 })
+
+test_that("scale_symbol_manual works", {
+  vdiffr::expect_doppelganger("geom_symbol_plot_scale",
+                              ggplot2::ggplot(data = iris,
+                                              ggplot2::aes(x = Sepal.Length,
+                                                           y = Petal.Length)) +
+                                geom_symbol(aes(symbol = Species)) +
+                                scale_symbol_manual(values = c(3, 1, 8)))
+})
