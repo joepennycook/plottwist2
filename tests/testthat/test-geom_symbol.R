4d7e355bfdec42ec89751c0eadc6f2ea1ce6e476
ea24925 <joseph.pennycook@bristol.ac.uk>
2025-03-06 13:03:05 +0000

    basic test for geom_symbol()

---
+++ tests/testthat/test-geom_symbol.R
@@ -0,0 +1,7 @@
+test_that("geom_symbol works", {
+  vdiffr::expect_doppelganger("geom_symbol_plot",
+                              ggplot2::ggplot(data = mtcars,
+                                              ggplot2::aes(x = cyl, y = mpg)) +
+                                ggplot2::geom_point() +
+                                theme_simple())
+})
