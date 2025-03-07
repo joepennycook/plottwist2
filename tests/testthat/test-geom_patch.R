ec0c26f2f02f9097dd6b1c15e18d82064451c2dc
ea24925 <joseph.pennycook@bristol.ac.uk>
2025-03-07 17:54:01 +0000

    geom_patch() test

---
+++ tests/testthat/test-geom_patch.R
@@ -0,0 +1,8 @@
+test_that("geom_patch works", {
+  vdiffr::expect_doppelganger("geom_patch_plot",
+                              ggplot2::ggplot(data.frame("x" = c(0, 1),
+                                                         "y" = c(0, 1)),
+                                              ggplot2::aes(x = x,
+                                                  y = y,)) +
+                                geom_patch(xmin = 0.2, xmax = 0.8, ymin = 0.2, ymax = 0.8))
+})
