df5bb558b4b28ae21e6d7183b156e30851d84e5a
ea24925 <joseph.pennycook@bristol.ac.uk>
2025-03-11 16:04:01 +0000

    update symbol scale tests

--- tests/testthat/test-geom_symbol.R
+++ tests/testthat/test-geom_symbol.R
@@ -5,11 +5,19 @@ test_that("geom_symbol works", {
                                 geom_symbol())
 })
 
-test_that("scale_symbol_manual works", {
-  vdiffr::expect_doppelganger("geom_symbol_plot_scale",
+test_that("default scale works", {
+  vdiffr::expect_doppelganger("geom_symbol_plot_default_scale",
                               ggplot2::ggplot(data = iris,
                                               ggplot2::aes(x = Sepal.Length,
                                                            y = Petal.Length)) +
-                                geom_symbol(aes(symbol = Species)) +
-                                scale_symbol_manual(values = c(3, 1, 8)))
+                                geom_symbol(ggplot2::aes(symbol = Species)))
+})
+
+test_that("scale_symbol_manual works", {
+  vdiffr::expect_doppelganger("geom_symbol_plot_scale_manual",
+                              ggplot2::ggplot(data = iris,
+                                              ggplot2::aes(x = Sepal.Length,
+                                                           y = Petal.Length)) +
+                                geom_symbol(ggplot2::aes(symbol = Species)) +
+                                scale_symbol_manual(values = c(26, 29, 31)))
 })
