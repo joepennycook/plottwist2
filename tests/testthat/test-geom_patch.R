a88dd6e981064672f7a8ae04d3737333bc9fa319
joepennycook <joe.pennycook@hotmail.co.uk>
2025-03-23 14:11:09 +0000

    tidy

--- tests/testthat/test-geom_patch.R
+++ tests/testthat/test-geom_patch.R
@@ -4,5 +4,6 @@ test_that("geom_patch works", {
                                                          "y" = c(0, 1)),
                                               ggplot2::aes(x = x,
                                                   y = y,)) +
-                                geom_patch(xmin = 0.2, xmax = 0.8, ymin = 0.2, ymax = 0.8))
+                                geom_patch(xmin = 0.2, xmax = 0.8,
+                                           ymin = 0.2, ymax = 0.8))
 })
