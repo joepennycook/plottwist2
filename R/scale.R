62a1181f3d70e6e797e9c0a89412443a9c12e44f
ea24925 <joseph.pennycook@bristol.ac.uk>
2025-05-15 16:14:29 +0100

    pattern scales

--- R/scale.R
+++ R/scale.R
@@ -27,6 +27,53 @@ pal_symbol <- function(n) {
       34, 14, 22, 30, 38,
       16, 24, 32, 40)[seq_len(n)]
   } else {
-    warn("More than 24 symbols requested. Consider specifying symbols manually.")
+    warn("More than 24 symbols required. Consider specifying symbols manually.")
+  }
+}
+
+#' @import ggplot2
+#' @export
+scale_pattern <- function(name = waiver(), ..., aesthetics = "pattern") {
+  discrete_scale(aesthetics, name = name, palette = pal_pattern)
+}
+
+#' @export
+scale_pattern_discrete <- scale_pattern
+
+pal_pattern <- function(n) {
+  if (n <= 282) {
+    c(c(c(1, 11, 3, 14, 9, 4, 13,
+          38, 5, 24, 27, 39, 8, 20, 25,
+          12, 2, 16, 34, 18, 32, 35, 22,
+          28, 40, 15, 19, 30, 37,
+          51, 44, 54, 49, 45, 53,
+          46, 64, 47, 60, 52, 42, 56,
+          58, 62, 55, 59),
+        c(11, 3, 14, 9, 4, 13,
+          38, 5, 24, 27, 39, 8, 20, 25,
+          12, 2, 16, 34, 18, 32, 35, 22,
+          28, 40, 15, 19, 30, 37,
+          51, 44, 54, 49, 45, 53,
+          46, 64, 47, 60, 52, 42, 56,
+          58, 62, 55, 59) + 64,
+        c(11, 3, 14, 9, 4, 13,
+          38, 5, 24, 27, 39, 8, 20, 25,
+          12, 2, 16, 34, 18, 32, 35, 22,
+          28, 40, 15, 19, 30, 37,
+          51, 44, 54, 49, 45, 53,
+          46, 64, 47, 60, 52, 42, 56,
+          58, 62, 55, 59) + 128),
+      c(c(38, 24, 27, 39, 8, 20, 25,
+          12, 34, 18, 32, 35, 22,
+          28, 40, 19, 30, 37),
+        c(38, 24, 27, 39, 8, 20, 25,
+          12, 34, 18, 32, 35, 22,
+          28, 40, 19, 30, 37) + 64,
+        c(11, 3, 14, 9, 13,
+          38, 24, 27, 39, 8, 20, 25,
+          12, 16, 34, 18, 32, 35, 22,
+          28, 40, 15, 19, 30, 37) + 128) + 192)
+  } else {
+    warn("More than 197 symbols requested. Consider specifying patterns manually.")
   }
 }
